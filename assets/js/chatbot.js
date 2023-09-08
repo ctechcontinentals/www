a440fd329f7141774a5a0afc1aec49efe5959357
cmulay <codewithchin@gmail.com>
2023-09-08 17:03:03 +0530

    Added Chatbot

---
+++ assets/js/chatbot.js
@@ -0,0 +1,9 @@
+(function (w, d) {
+  w.CollectId = "64faf88617960a15b2335ee3";
+  var h = d.head || d.getElementsByTagName("head")[0];
+  var s = d.createElement("script");
+  s.setAttribute("type", "text/javascript");
+  s.async = true;
+  s.setAttribute("src", "https://collectcdn.com/launcher.js");
+  h.appendChild(s);
+})(window, document);
